844739a2f7cfbf9f363fc95a3753a38d4c19f1c9
Paul Pieper <paulmpieper@gmail.com>
2018-02-11 23:44:07 -0500

    -basic JSON parsing has been implemented -dispays an image and a name now after scanning -todo: use groceryList models to save item to a chosen grocery list

--- barcodeScan/apps.py
+++ barcodeScan/apps.py
@@ -1,3 +1,5 @@
 from django.apps import AppConfig
 
 
+class barcodeScanConfig(AppConfig):
+    name = 'barcodeScan'
--- barcodeScan/urls.py
+++ barcodeScan/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 
 from . import views
 
+app_name = 'barcodeScan'
 urlpatterns = [
     path('', views.index, name='index'),
+    path('confirm/', views.confirm, name='confirm')
 ]
