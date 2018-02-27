a9079c94b2ecb9796a7925f41cc6d2fe3b2176e6
brendanshanahan <brendan.t.shanahan@gmail.com>
2018-02-27 16:57:25 -0500

    skeleton data model incorporating recipes; mostly works

--- groceryList/models.py
+++ groceryList/models.py
@@ -1,8 +1,20 @@
 from django.db import models
+from django.forms import ModelForm
+from django.urls import reverse
+#from django.utils import timezone
+#import datetime
+
+## do you need to forward-define functions/classes in Pyhton? is there
+## a way to reference Recipe() inside GroceryList() without this helper?
+#class RecipeForward(models.Model):
+#    Recipe()
 
 class GroceryList(models.Model):
     name = models.CharField(max_length=100)
     date = models.DateTimeField(null=True, blank=True)
+    
+    fooditems = models.ManyToManyField('FoodItem')
+    recipes = models.ManyToManyField('Recipe')
 
     def __str__(self):
     	return self.name
@@ -10,15 +22,20 @@ class GroceryList(models.Model):
     pass
 
 class FoodItem(models.Model):
-    name = models.CharField(max_length=100)
+    name = models.CharField(max_length = 100, blank=True)
     quantity = models.IntegerField(default=1)
     date = models.DateTimeField(null=True, blank=True)
-    
-    lists = models.ManyToManyField(GroceryList)
     
     def __str__(self):
         return self.name
     
-#	groceryListId = models.IntegerField()
-#	foodItemId = models.IntegerField()
-#	quantity = models.IntegerField()
+class Recipe(models.Model):
+    name = models.CharField(max_length=100, blank=True)
+    date = models.DateTimeField(null=True, blank=True)
+    
+    fooditems = models.ManyToManyField(FoodItem)
+    
+    def __str__(self):
+        return self.name
+    
+    pass
