06658ec5bd6fb9178f5fa4d64acbee66e5613c10
finn17 <paddleball17@gmail.com>
2018-04-23 22:48:27 -0400

    Some missing information since a git fetch and reset was messy.

--- groceryList/views.py
+++ groceryList/views.py
@@ -9,19 +9,19 @@ from django.db.models import F
 from django.views.generic.edit import CreateView
 from .models import GroceryList, FoodItem, Recipe
 from . import forms
-   
+
 
 #from django.contrib.auth.decorators import login_required
 #@login_required(login_url='/accounts/login/')
 class IndexView(ListView):
     model = GroceryList
     template_name = 'groceryList/index.html'
-    
+
     def get_context_data(self, **kwargs):
         context = super(IndexView, self).get_context_data(**kwargs)
         context['all_grocery_lists'] = GroceryList.objects.all()
         context['all_recipes'] = Recipe.objects.all()
-        
+
         return context
 
 class NewGroceryListView(CreateView):
@@ -41,35 +41,35 @@ class NewRecipeView(CreateView):
     form_class_new = forms.AddRecipeForm
 
     template_name = 'groceryList/new_recipe.html'
-  
+
 class GroceryListView(DetailView):
     model = GroceryList
-    template_name = 'groceryList/detail.html'   
-   
+    template_name = 'groceryList/detail.html'
+
     def get_context_data(self, **kwargs):
         context = super(GroceryListView, self).get_context_data(**kwargs)
         context['item_form'] = forms.AddItemToListForm
         context['recipe_form'] = forms.AddRecipeToListForm
-            
+
         return context
 
 class RecipeView(DetailView):
     model = Recipe
-    template_name = 'groceryList/recipe.html'   
-   
+    template_name = 'groceryList/recipe.html'
+
     def get_context_data(self, **kwargs):
         context = super(RecipeView, self).get_context_data(**kwargs)
         context['form'] = forms.AddItemToRecipeForm
-            
+
         return context
-    
+
 def add(request):
     if request.method == "POST":
         form = forms.AddGroceryListForm(request.POST)
 
         if form.is_valid():
             text = form.cleaned_data['name']
-            
+
             # *for the special case of scanning an item when there are no lists--
             # create a new list from the barcodeScan app and add the scanned item
             # to it automatically.
@@ -77,11 +77,11 @@ def add(request):
 
             if GroceryList.objects.filter(name=text).exists():
                 messages.warning(request, "Grocery list already exists")
-                
+
                 return HttpResponseRedirect(reverse('groceryList:new'))
-            
+
             # redirect to new grocery list after creation
-            else:        
+            else:
                 new_list = GroceryList.objects.create(name=text, date=timezone.now())
 
                 # *if we are making a list from the barcodeScan app, add the food item too
@@ -100,21 +100,21 @@ def add(request):
 def add_recipe(request):
     if request.method == "POST":
         form = forms.AddRecipeForm(request.POST)
-        
+
         if form.is_valid():
             text = form.cleaned_data['name']
 
             if Recipe.objects.filter(name=text).exists():
                 messages.warning(request, "Recipe already exists")
-                
+
                 return HttpResponseRedirect(reverse('groceryList:new_recipe'))
-            
+
             # redirect to new recipe after creation
-            else:        
+            else:
                 new_recipe = Recipe.objects.create(name=text, date=timezone.now())
-                
+
                 return HttpResponseRedirect(reverse('groceryList:recipe', args = (new_recipe.id,)))
-    
+
     # if method = GET, then return a blank form
     else:
         form = forms.AddRecipeForm()
@@ -122,10 +122,10 @@ def add_recipe(request):
 
 def add_to_list(request, pk):
     grocery_list = get_object_or_404(GroceryList, pk = pk)
-    
+
     if request.method == "POST":
         form = forms.AddItemToListForm(request.POST)
-        
+
         if form.is_valid():
             food = form.cleaned_data['food_item']
             qty = form.cleaned_data['quantity']
@@ -137,13 +137,13 @@ def add_to_list(request, pk):
                 new_food = FoodItem(name=food, quantity = qty, date=timezone.now())
                 new_food.save()
                 grocery_list.fooditems.add(new_food)
-            
+
         return HttpResponseRedirect(reverse('groceryList:detail', args = (pk,)))
-            
+
 
     else:
         form = forms.AddItemToListForm()
-    
+
     return render(request, 'groceryList/detail.html', {'form': form})
 
 def increment_food_item(request):
@@ -151,7 +151,7 @@ def increment_food_item(request):
     if request.method == 'GET':
         pk = request.GET['list_id']
         food_item = request.GET['food_item']
-    
+
     if pk:
         grocery_list = get_object_or_404(GroceryList, pk = pk)
         qty = grocery_list.fooditems.get(name=food_item).quantity
@@ -165,87 +165,99 @@ def decrement_food_item(request):
     if request.method == 'GET':
         pk = request.GET['list_id']
         food_item = request.GET['food_item']
-    
+
     if pk:
         grocery_list = get_object_or_404(GroceryList, pk = pk)
         qty = grocery_list.fooditems.get(name=food_item).quantity
-        
+
         # this doesn't work; probably tricky to make it happen here
         if qty == 0:
             grocery_list.fooditems.remove(food_item)
             grocery_list.save()
-        
+
         else:
             grocery_list.fooditems.filter(name=food_item).update(quantity = F('quantity') - 1)
             return HttpResponse(qty - 1)
-        
+
+        # changes here from Finn
+            try:
+                food_item = FoodItem.objects.get(name=food_item)
+                stat_item = Consumed_Stats.objects.get(food=food_item)
+                stat_item.count1 += 1
+                stat_item.save()
+            except Consumed_Stats.DoesNotExist:
+                new_stat_item = Consumed_Stats(food = food_item, count1 = 1,
+                count2 = 0, count3 = 0, count4 = 0)
+                new_stat_item.save()
+            # end changes
+
     return HttpResponse(0)
 
 def add_recipe_to_list(request, pk):
     grocery_list = get_object_or_404(GroceryList, pk = pk)
-    
+
     if request.method == "POST":
         form = forms.AddRecipeToListForm(request.POST)
-        
+
         if form.is_valid():
             recipe = form.cleaned_data['recipe_name']
-            
+
             if Recipe.objects.filter(name=recipe).exists():
-                
+
                 # probably a good idea to ask the user if they want to do this
                 if not grocery_list.recipes.filter(name=recipe).exists():
                     grocery_list.recipes.add(Recipe.objects.get(name=recipe))
-                
+
                 # this too maybe? do we want to add the entire quantity that the recipe
                 # calls for, or just the difference between that and what's already
                 # on the user's grocery list?
                 for food_item in Recipe.objects.get(name=recipe).fooditems.all():
-                    
+
                     if grocery_list.fooditems.filter(name=food_item).exists():
                         grocery_list.fooditems.filter(name=food_item).update(quantity = F('quantity') + food_item.quantity)
-                        
+
                     else:
                         new_food = FoodItem(name=food_item, quantity = food_item.quantity, date=timezone.now())
                         new_food.save()
                         grocery_list.fooditems.add(new_food)
-                                
+
                 messages.warning(request, "Grocery list items updated")
-                
+
 
 #                # probably a good idea to ask the user if they want to do this
 #                if grocery_list.recipes.filter(name=recipe).exists():
-#                    
+#
 #                    for food_item in Recipe.objects.filter(name=recipe):
 #                        on_hand = grocery_list.fooditems.get(name=food_item.name).quantity()
 #                        if on_hand < food_item.quantity:
 #                            diff = food_item.quantity - on_hand
 #                            grocery_list.fooditems.filter(name=food_item).update(quantity = F('quantity') + diff)
-#                                
+#
 #                    messages.warning(request, "Grocery list items updated")
-#    
+#
 #                else:
 #                    # have to use .get() instead of .filter() ??? something about QuerySets
 #                    grocery_list.recipes.add(Recipe.objects.get(name=recipe))
-            
+
             else:
                 new_recipe = Recipe(name=recipe, date=timezone.now())
                 new_recipe.save()
                 grocery_list.recipes.add(new_recipe)
-            
+
         return HttpResponseRedirect(reverse('groceryList:detail', args = (pk,)))
-            
+
 
     else:
         form = forms.AddRecipeToListForm()
-    
+
     return render(request, 'groceryList/detail.html', {'form': form})
 
 def add_to_recipe(request, pk):
     recipe = get_object_or_404(Recipe, pk = pk)
-    
+
     if request.method == "POST":
         form = forms.AddItemToRecipeForm(request.POST)
-        
+
         if form.is_valid():
             food = form.cleaned_data['food_item']
 
@@ -254,19 +266,19 @@ def add_to_recipe(request, pk):
                 # probably a good idea to ask the user if they want to do this
                 if recipe.fooditems.filter(name=food).exists():
                     recipe.fooditems.filter(name=food).update(quantity = F('quantity') + 1)
-    
-                else:            
+
+                else:
                     recipe.fooditems.add(FoodItem.objects.filter(name=food))
-            
+
             else:
                 new_food = FoodItem(name=food, date=timezone.now())
                 new_food.save()
                 recipe.fooditems.add(new_food)
-            
+
         return HttpResponseRedirect(reverse('groceryList:recipe', args = (pk,)))
-            
+
 
     else:
         form = forms.AddItemToListForm()
-    
+
     return render(request, 'groceryList/recipe.html', {'form': form})
