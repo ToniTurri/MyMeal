0d08cc360011344803c5eddd23290f9c81a236e3
ToniTurri <tturri23@hotmail.com>
2018-04-29 19:35:03 -0400

    Recipe changes
    deletion, beginning of editing, base recipes page has styling

--- recipes/urls.py
+++ recipes/urls.py
@@ -5,6 +5,7 @@ app_name = 'recipes'
 urlpatterns = [
     path('', views.IndexView.as_view(), name='index'),
     path('<int:pk>/', views.RecipeView.as_view(), name='detail'),
-    #path('add_recipe/', views.add_recipe, name='add_recipe'),
     path('new_recipe/', views.add_recipe, name='new_recipe'),
+    path('<int:pk>/edit', views.edit_recipe, name='edit_recipe'),
+    path('<int:pk>/delete', views.delete_recipe, name='delete_recipe')
 ]
