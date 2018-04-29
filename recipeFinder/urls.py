a06db4f95bb6faa9eb72a95881ea2a19a81bc198
Paul Pieper <paulmpieper@gmail.com>
2018-04-29 18:04:30 -0400

    Categorie can now be saved to the DB from Yummly --also switched images to use static url

--- recipeFinder/urls.py
+++ recipeFinder/urls.py
@@ -5,6 +5,7 @@ from . import views
 app_name = 'recipeFinder'
 urlpatterns = [
     path('', views.index, name='index'),
+    path('detail/<str:id>/<str:course>/', views.recipe_detail, name='recipe_detail'),
     path('detail/<str:id>', views.recipe_detail, name='recipe_detail'),
     path('inventory-check', views.inventoryCheck, name='inventory-check'),
     path('freedom', views.freeChoice, name='freedom'),
