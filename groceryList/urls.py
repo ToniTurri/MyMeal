f0913d053c7b9ea531656800a050f054292e4d66
Paul Pieper <paulmpieper@gmail.com>
2018-05-02 18:56:00 -0400

    Fixed urlconf for groceryList

--- groceryList/urls.py
+++ groceryList/urls.py
@@ -5,11 +5,9 @@ from . import views
 app_name = 'groceryList'
 urlpatterns = [
     path('', views.IndexView.as_view(), name='index'),
-    path('new/', views.NewGroceryListView.as_view(), name='new'),
-    path('new/<str:food_name>', views.NewGroceryListView.as_view(), name='new'),
-    path('add/', views.add, name='add'),
     path('<int:pk>/', views.GroceryListView.as_view(), name='detail'),
     path('new/', views.NewGroceryListView.as_view(), name='new'),
+    path('new/<str:food_name>', views.NewGroceryListView.as_view(), name='new'),
     path('add/', views.add, name='add'),
     path('<int:pk>/update', views.update, name='update'),
     path('<int:pk>/<int:id>/confirm', views.confirm_item, name='confirm_item'),
