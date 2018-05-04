299a0e631fde9e029bde383ff1912eb11b3747d2
finn17 <paddleball17@gmail.com>
2018-05-04 01:30:53 -0400

    accidentally put the whole meal app inside templates, this removes it

--- templates/barcodeScan/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class barcodeScanConfig(AppConfig):
-    name = 'barcodeScan'
--- templates/barcodeScan/models.py
+++
@@ -1,7 +0,0 @@
-from django.db import models
-
-#class Barcode(models.Model):
-#    number = models.IntegerField()
-#
-#    def __str__(self):
-#    	return self.number
