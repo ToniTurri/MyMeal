47150f05815515d59c8618a7e8af08403d312421
finn17 <paddleball17@gmail.com>
2018-05-08 20:45:13 -0400

    Deleted file

--- stats/forms.py
+++
@@ -1,7 +0,0 @@
-from django import forms
-from django.forms import ChoiceField
-
-
-class FilterForm(forms.Form):
-    CHOICES = (('1', 'Name',), ('2', 'Most Consumed',), ('3', 'Least Consumed',))
-    choice_field = forms.ChoiceField(choices=CHOICES)
